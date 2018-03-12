54ef1cd1f09a6d55e2c26e6ff938ed81b55d5ba8
Khai Nguyen <khai.nguyen@cudirect.com>
2018-03-12 14:05:12 -0700

    Add GradeBookType

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,11 @@
+﻿namespace GradeBook.Enums
+{
+    public enum GradeBookType
+    {
+        Standard = 1,
+        Ranked = 2,
+        ESNU = 3,
+        OneToFour = 4,
+        SixPoint = 5
+    }
+}
